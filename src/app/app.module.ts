3fa91ecc33863ded2aaa528143df6ba3f6305789
Dorina Ferencsik <dorina.ferencsik@gmail.com>
2020-08-01 19:54:18 +0200

    Feat: added basic layout, mixin and responsivity

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FlexLayoutModule } from '@angular/flex-layout';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -16,7 +17,8 @@ import { HttpClientModule } from '@angular/common/http';
     AppRoutingModule,
     BrowserAnimationsModule,
     HttpClientModule,
-    MainModule
+    MainModule,
+    FlexLayoutModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
